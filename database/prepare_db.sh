2813ea10b8b50fe9a354aa8d611e415815df257d
elfourbo <elfourbo@42madrid.com>
2022-02-27 19:54:05 +0100

    mysql container working and setup (env for build is a little dirty :/)

---
+++ database/prepare_db.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+mkdir -p /var/lib/mysql/
+
+chown -R mysql: /var/lib/mysql
+
+if [ ! -f /var/lib/mysql/ibdata1 ]; then
+
+    mysql_install_db --user=mysql --datadir="/var/lib/mysql"
+
+    /usr/bin/mysqld_safe --defaults-file=/etc/mysql/my.cnf &
+    sleep 2
+
+    mysql -u root --password="" -e "CREATE DATABASE ${DB}"
+    echo "USE ${DB};
+            GRANT ALL ON *.* TO '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASS}';
+            GRANT ALL ON *.* TO '${DB_USER}'@'localhost' IDENTIFIED BY '${DB_PASS}';
+            FLUSH PRIVILEGES;" | mysql -u root --password=""
+
+    sleep 10
+    pkill mariadbd
+fi
