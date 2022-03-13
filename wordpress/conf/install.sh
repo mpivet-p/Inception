4bf0d8edef959580aabe0d87d1caca81cc51ca45
elfourbo <elfourbo@42madrid.com>
2022-03-13 18:21:48 +0100

    wp-cli doesn't work if /var/www/html is a docker volume. Because using user www-data prevent wp-cli to read/write

---
+++ wordpress/conf/install.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#Exit if one command fails
+set -e
+
+if [ ! -f "/var/www/html/wp-config.php" ]; then
+    wp core download
+    wp config create\
+        --dbhost=${DB_HOST}\
+        --dbname=${DB_NAME}\
+        --dbuser=${DB_USER}\
+        --dbpass=${DB_PASS}
+
+    wp core install\
+        --url=${WP_URL}\
+        --title=${WP_TITLE}\
+        --admin_name=${WP_ADMIN}\
+        --admin_password=${WP_PASS}\
+        --admin_email=${WP_MAIL}
+
+    cd wp-content && mkdir uploads && chgrp web uploads/ && chmod 775 uploads/
+fi
+
+/usr/sbin/php-fpm7.3 --nodaemonize
