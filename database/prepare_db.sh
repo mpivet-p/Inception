8d02ff561aa315d88da89e06a9def69056de898b
elfourbo <elfourbo@42madrid.com>
2022-03-19 19:36:03 +0100

    php-fpm/nginx issue with document_root

--- database/prepare_db.sh
+++ database/prepare_db.sh
@@ -10,8 +10,8 @@ if [ ! -f /var/lib/mysql/ibdata1 ]; then
     /usr/bin/mysqld_safe --defaults-file=/etc/mysql/my.cnf &
     sleep 2
 
-    mysql -u root --password="" -e "CREATE DATABASE ${DB}"
-    echo "USE ${DB};
+    mysql -u root --password="" -e "CREATE DATABASE ${DB_NAME}"
+    echo "USE ${DB_NAME};
             GRANT ALL ON *.* TO '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASS}';
             GRANT ALL ON *.* TO '${DB_USER}'@'localhost' IDENTIFIED BY '${DB_PASS}';
             FLUSH PRIVILEGES;" | mysql -u root --password=""
--- wordpress/conf/install.sh
+++ wordpress/conf/install.sh
@@ -2,9 +2,11 @@
 
 #Exit if one command fails
 set -e
+echo $DB_HOST $DB_NAME $DB_USER $DB_PASS $WP_URL $WP_TITLE $WP_ADMIN $WP_PASS $WP_MAIL
 
-if [ ! -f "/var/www/wordpress/wp-config.php" ]; then
-    wp core download
+export WP_CLI_CACHE_DIR=/var/www/html/.wp-cli/cache
+if [ ! -f "/var/www/html/wordpress/wp-config.php" ]; then
+    wp core download --path="/var/www/html"
     wp config create\
         --dbhost=${DB_HOST}\
         --dbname=${DB_NAME}\
@@ -18,7 +20,6 @@ if [ ! -f "/var/www/wordpress/wp-config.php" ]; then
         --admin_password=${WP_PASS}\
         --admin_email=${WP_MAIL}
 
-    cd wp-content && mkdir uploads && chgrp web uploads/ && chmod 775 uploads/
 fi
 
 /usr/sbin/php-fpm7.3 --nodaemonize
